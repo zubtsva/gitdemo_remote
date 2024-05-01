3ba8ec28643c44206fe27a1be682d8c45912685f
lz <l.zubtsova@mail.ru>
2024-05-01 12:32:21 +0300

    Initial Commit

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Лена Зубцова on 1.05.24.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
